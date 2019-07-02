a9b1f45e5a2779b7a2429c2deff11f54c148fb04
Muhammet Mustafa Tozlu <m.mustafatozlu@gmail.com>
2019-07-02 10:33:49 +0300

    Excel Dosyası Okuma Projesi Tamamlandı.

---
+++ ExcelDosyaOkuma/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace ExcelDosyaOkuma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDosyaSec = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnDosyaSec
+            // 
+            this.btnDosyaSec.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnDosyaSec.Location = new System.Drawing.Point(12, 12);
+            this.btnDosyaSec.Name = "btnDosyaSec";
+            this.btnDosyaSec.Size = new System.Drawing.Size(131, 58);
+            this.btnDosyaSec.TabIndex = 0;
+            this.btnDosyaSec.Text = "Dosya Seç";
+            this.btnDosyaSec.UseVisualStyleBackColor = true;
+            this.btnDosyaSec.Click += new System.EventHandler(this.BtnDosyaSec_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(149, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(639, 416);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 437);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnDosyaSec);
+            this.Name = "Form1";
+            this.Text = "Excel Dosyası Okuma";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDosyaSec;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ ExcelDosyaOkuma/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using ExcelApp = Microsoft.Office.Interop.Excel;
+
+namespace ExcelDosyaOkuma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnDosyaSec_Click(object sender, EventArgs e)
+        {
+            string DosyaYolu;
+            string DosyaAdi;
+            DataTable dt;
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "Excel Dosyası | *.xls; *.xlsx; *.xlsm";
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                DosyaYolu = file.FileName;// seçilen dosyanın tüm yolunu verir
+                DosyaAdi = file.SafeFileName;// seçilen dosyanın adını verir.
+                ExcelApp.Application excelApp = new ExcelApp.Application();
+                if (excelApp == null)
+                { //Excel Yüklümü Kontrolü Yapılmaktadır.
+                    MessageBox.Show("Excel yüklü değil.");
+                    return;
+                }
+
+                //Excel Dosyası Açılıyor.
+                ExcelApp.Workbook excelBook = excelApp.Workbooks.Open(DosyaYolu);
+                //Excel Dosyasının Sayfası Seçilir.
+                ExcelApp._Worksheet excelSheet = excelBook.Sheets[1];
+                //Excel Dosyasının ne kadar satır ve sütun kaplıyorsa tüm alanları alır.
+                ExcelApp.Range excelRange = excelSheet.UsedRange;
+
+                int satirSayisi = excelRange.Rows.Count; //Sayfanın satır sayısını alır.
+                int sutunSayisi = excelRange.Columns.Count; //Sayfanın sütun sayısını alır.
+                dt = ToDataTable(excelRange, satirSayisi, sutunSayisi);
+
+                dataGridView1.DataSource = dt;
+                dataGridView1.Refresh();
+
+                //Okuduktan Sonra Excel Uygulamasını Kapatıyoruz.
+                excelApp.Quit();
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
+
+            }
+            else
+            {
+                MessageBox.Show("Dosya Seçilemedi.");
+            }
+        }
+
+        public DataTable ToDataTable(ExcelApp.Range range, int rows, int cols)
+        {
+            DataTable table = new DataTable();
+
+            for (int i = 1; i <= rows; i++)
+            {
+                if (i == 1)
+                { // ilk satırı Sutun Adları olarak kullanıldığından bunları Sutün Adları Olarak Kaydediyoruz.
+                    for (int j = 1; j <= cols; j++)
+                    {
+                        //Sütunların içeriği boş mu kontrolü yapılmaktadır.
+                        if (range.Cells[i, j] != null && range.Cells[i, j].Value2 != null)
+                            table.Columns.Add(range.Cells[i, j].Value2.ToString());
+                        else
+                            table.Columns.Add(j.ToString() + ".Sütun"); //Boş olduğunda Kaçınsı Sutünsa Adı veriliyor.
+                    }
+                    continue;
+                }
+
+                //Yukarıda Sütunlar eklendi onun şemasına göre yeni bir satır oluşturuyoruz. 
+                //Okunan verileri yan yana sıralamak için
+                var yeniSatir = table.NewRow();
+                for (int j = 1; j <= cols; j++)
+                {
+                    //Sütunların içeriği boş mu kontrolü yapılmaktadır.
+                    if (range.Cells[i, j] != null && range.Cells[i, j].Value2 != null)
+                        yeniSatir[j - 1] = range.Cells[i, j].Value2.ToString();
+                    else
+                        yeniSatir[j - 1] = String.Empty; // İçeriği boş hücrede hata vermesini önlemek için
+                }
+                table.Rows.Add(yeniSatir);
+
+            }
+            return table;
+        }
+    }
+}
